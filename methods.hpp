1d317c57346b840b61a317bc75cdb9036c1285e6
SteppenTrubsal <Niko@Lenovo.localdomain>
2024-03-12 21:59:15 +0300

    Made fixed-point method but wrong

--- methods.hpp
+++ methods.hpp
@@ -1,20 +1,46 @@
 #include "matrix.hpp"
 using namespace std;
 
-int fixedPointMethod(matrix A, double* b, double* xk0, double tau, double eps, int bLen = 4, int xk0Len = 4){
+double fixedPointMethod(matrix A, double* _b, double* x0, double tau, double eps, int bLen = 4, int xk0Len = 4){
     matrix E(A.getDim());
-    double *xk1 = new double[xk0Len];
+    double *xk0 = new double[xk0Len];
+    cout << "xk0: ";
+    for(int i = 0; i < xk0Len; i++){
+        xk0[i] = x0[i];
+        cout << xk0[i] << " ";
+    }
+    cout << endl;
+    
+    double *b = new double[xk0Len];
+    cout << "b: ";
+    for(int i = 0; i < xk0Len; i++){
+        b[i] = _b[i];
+        cout << b[i] << " ";
+    }
+    cout << endl;
     double *diff = new double[xk0Len];
 
+    (E - A*tau).show();
+
+    double gg = (E - A * tau).getNorm2();
+	//if (gg > 1) { return gg; }
+    double counter = 0;
+
     do {
-        double gg = (E - A * tau).getNorm2();
-		if (gg < 1) { return 0; }
-		else { return gg; }
-
-
-
+        double *xk1 = new double[xk0Len];
+        for(int i = 0; i < xk0Len; i++){
+            xk1[i] = gg*xk0[i] + tau*b[i];
+        }
+        
+        cout << "diff: ";
         for(int i = 0; i < xk0Len; i++) {
             diff[i] = xk0[i] - xk1[i];
+            cout << diff[i] << " ";
         }
-	} while (getEucleadeanNorm(diff) < eps);
+        delete[] xk0;
+        xk0 = xk1;
+        counter++;
+        cout << counter << " " << getEucleadeanNorm(diff) << endl;
+	} while (getEucleadeanNorm(diff) > eps);
+    return counter;
 }
--- src.hpp
+++ src.hpp
@@ -1,15 +1,14 @@
-int _A[4][4] = {
+double _A[4][4] = {
 	{18,8,6,1},
 	{4,13,8,1},
 	{0,9,14,5},
 	{6,4,4,12}
 };
 
-int _b[] = { 17,5,21,0 };
+double _b[] = { 17,5,21,0 };
 
-double _tau = 0.025;
-
-int _x0[] = {1,1,1};
+double _tau = 0.05;
+double _x0[] = {1,1,1,1};
 
 double** createA(int dim = 4) {
 	double** A = new double* [dim];
