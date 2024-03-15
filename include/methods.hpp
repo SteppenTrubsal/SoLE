1d781bebcfd13530ddfdb1d17feaba8153eb526f
Ёж Соник <g.mens.hl@gmail.com>
2024-03-15 18:12:24 +0300

    fix int Jacobi(matrix A, vector<double> b,vector<double> x, double eps)

--- include/methods.hpp
+++ include/methods.hpp
@@ -1,24 +1,38 @@
+//methods.hpp
+
 #include "matrix.hpp"
 using namespace std;
 
-int Jacobi(matrix A, vector<double> b, vector<double> x, double eps){
+int Jacobi(matrix A, vector<double> b,vector<double> x, double eps){
     vector<matrix> LUD = A.getLUD();
     vector<double> diff;
+    vector<double> allDiff;
     int counter = 0;
     do{
+
         vector<double> temp1((LUD[0]+LUD[1])*x);
         vector<double> temp2(x.size());
         for(int i = 0; i < x.size(); i++){
             temp2[i] = b[i] - temp1[i];
         }
         vector<double> x1 = LUD[2].getReverse() * temp2;
+        diff.clear();
         for(int i = 0; i < x1.size(); i++){
-            diff[i] = x[i] - x1[i];
+            diff.push_back(x[i] - x1[i]);
         }
         x.clear();
-        vector<double> x= x1;
-        
+        x= x1;
+        std::copy(diff.begin(), diff.end(), std::back_inserter(allDiff));
         counter++;
     }while(getEucleadeanNorm(diff) > eps);
+    int i = 0;
+    for (auto d : allDiff)
+    {
+        i++;
+        cout << d << ' ';
+        if (i%4 == 0)
+            cout << endl;
+    }
+    cout << endl;
     return counter;
 }
